983c457fbae592ed54a840f1a24b51775015f245
arimariojesus <arimario.jesus@hotmail.com>
2022-06-13 23:37:34 -0300

    feat: add lib declartion types

---
+++ src/use-search.d.ts
@@ -0,0 +1,30 @@
+declare module 'use-search' {
+  import React, { ChangeEvent, Dispatch } from 'react';
+
+  export type Status = 'IDLE' | 'OK' | 'NOT_FOUND';
+
+  type Fields<T> = Array<keyof T>;
+
+  type SearchState<T> = {
+    status: Status;
+    data: T[];
+  };
+
+  export interface Options<T> {
+    debounce?: number;
+    initialQuery?: string;
+    fields?: Fields<T>;
+  }
+
+  export interface Return<T> {
+    data: T[];
+    status: Status;
+    query: string;
+    handleChange: (event: ChangeEvent<HTMLInputElement> | string) => void;
+    setQuery: Dispatch<React.SetStateAction<string>>;
+  }
+
+  const useSearch: <T>(collection: T[], options?: Options<T>) => Return<T>;
+
+  export default useSearch;
+}
