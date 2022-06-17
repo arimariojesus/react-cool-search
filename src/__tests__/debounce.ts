c05aa2b6fcca027f036ed20cf1dec6c832bc3395
arimariojesus <arimario.jesus@hotmail.com>
2022-06-17 00:06:31 -0300

    test: ensure trigger function just once

--- src/__tests__/debounce.ts
+++ src/__tests__/debounce.ts
@@ -1,15 +1,27 @@
 import debounce from '../debounce';
 
 describe('debounce', () => {
-  it('should trigger function on correct time', () => {
+  const fn = jest.fn();
+  const debouncedFn = debounce(fn, 100);
+
+  beforeEach(() => {
     jest.useFakeTimers();
-    const fn = jest.fn();
-    const debouncedFn = debounce(fn, 100);
+  });
+
+  it('should trigger function on correct time', () => {
     debouncedFn();
     expect(fn).not.toHaveBeenCalled();
     jest.advanceTimersByTime(50);
     expect(fn).not.toHaveBeenCalled();
     jest.advanceTimersByTime(50);
     expect(fn).toHaveBeenCalled();
+  });
+
+  it('should trigger function just once', () => {
+    for (let i = 0; i < 10; i += 1) {
+      debouncedFn();
+    }
+
+    expect(fn).toHaveBeenCalledTimes(1);
   });
 });
