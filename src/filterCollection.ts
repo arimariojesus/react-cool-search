a111533edc2499e899349e79261658d68ad939d4
arimariojesus <arimario.jesus@hotmail.com>
2022-09-11 15:38:24 -0300

    feat(filterCollection): get relevant properies from each item on collection

--- src/filterCollection.ts
+++ src/filterCollection.ts
@@ -1,6 +1,11 @@
 import { getDeepValue } from './getDeepValue';
 import { hasOnlySpaces } from './hasOnlySpaces';
+import { propertiesToArray } from './properiesToArray';
 import { DeepKey } from './types';
+
+const getRelevantFields = <T, K extends object>(fields: T, obj: K) => {
+  return Array.isArray(fields) ? fields : propertiesToArray(obj);
+};
 
 const normalizeValue = (value: any): string =>
   value
@@ -17,14 +22,19 @@ export const filterCollection = <
   collection: T[],
   fields: DeepKey<T, K>[] | null = null,
 ): T[] => {
-  if (hasOnlySpaces(query) || fields === null) {
+  if (Array.isArray(fields) && !fields.length) {
+    return [];
+  }
+
+  if (hasOnlySpaces(query)) {
     return collection;
   }
 
   const normalizedQuery = normalizeValue(query);
 
   return collection.filter(item => {
-    const someFieldMatches = fields.some(field => {
+    const relevantFields = getRelevantFields(fields, item) as DeepKey<T, K>[];
+    const someFieldMatches = relevantFields.some(field => {
       const value = getDeepValue(item, field);
       if (!!value && (typeof value === 'string' || typeof value === 'number')) {
         return normalizeValue(value).includes(normalizedQuery);
