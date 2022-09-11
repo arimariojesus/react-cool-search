7894af0832e004960508892f689cef64b0f80a04
arimariojesus <arimario.jesus@hotmail.com>
2022-09-11 12:43:07 -0300

    style: rename ValidValues type

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,20 @@
-type Value = string | number | Record<string, any> | Array<any>;
+type ValidValues =
+  | string
+  | number
+  | Record<string, any>
+  | Array<any>
+  | undefined
+  | null;
 
 export type DeepKey<
   T extends Record<string, any>,
   K extends string = string,
 > = K extends keyof T
-  ? T[K] extends Value
+  ? T[K] extends ValidValues
     ? K
     : never
   : K extends `${infer TKey}.${infer Rest}`
-  ? T[TKey] extends Value
+  ? T[TKey] extends ValidValues
     ? DeepKey<T[TKey], Rest> extends never
       ? never
       : K
