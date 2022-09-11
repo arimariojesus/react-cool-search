8730a468e155961005b16affa418e34ee2d8cc06
arimariojesus <arimario.jesus@hotmail.com>
2022-09-11 14:54:36 -0300

    feat: add propertiesToArray util

---
+++ src/properiesToArray.ts
@@ -0,0 +1,17 @@
+const isObject = (val: unknown): boolean =>
+  val instanceof Object && !Array.isArray(val);
+
+export const propertiesToArray = (obj: object): string[] => {
+  const addDelimiter = (a: string, b: string) => (a ? `${a}.${b}` : b);
+
+  const paths = (obj: Record<string, any> = {}, head = ''): string[] => {
+    return Object.entries(obj).reduce<string[]>((product, [key, value]) => {
+      const fullPath = addDelimiter(head, key);
+      return isObject(value)
+        ? product.concat(paths(value, fullPath))
+        : product.concat(fullPath);
+    }, []);
+  };
+
+  return paths(obj);
+};
